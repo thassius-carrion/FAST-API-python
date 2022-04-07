c8b96e0b49740a02fe0105b0e2de5924e09321f1
Thassius <thassius.carrion@hotmail.com>
2022-04-07 07:21:25 -0300

    modified main

--- src/main.py
+++ src/main.py
@@ -9,17 +9,27 @@ from infra.sqlalchemy.config.database import get_db, create_db
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 create_db()
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="htmldirectory")
+# permitir que minha API seja consumida, sem problema de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+#templates = Jinja2Templates(directory="htmldirectory")
 
 @app.get("/home", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return {"Msg": "Pagina inicial"}
 
 @app.get("/home/products")
 def get_products(db: Session = Depends(get_db)):
@@ -29,14 +39,10 @@ def get_products(db: Session = Depends(get_db)):
 @app.get("/home/products/{product_id}",  response_class=HTMLResponse)
 def get_products_by_id( product_id: int, request: Request, db: Session = Depends(get_db)):
     product = RepositoryProduct(db).get_by_id(product_id)
-    return templates.TemplateResponse("home.html", {"request": request, "product_id": product_id})
-    #return product
+    #return templates.TemplateResponse("home.html", {"request": request, "product_id": product_id})
+    return product
 
 @app.post("/home/products")
-async def  handle_form(ass: str = Form(...)):
-     print(ass)
-
-@app.post("/submitform")
 def  post_product(product: schemas.Product, db: Session = Depends(get_db)):
     product_created = RepositoryProduct(db).create(product)
     return product_created
